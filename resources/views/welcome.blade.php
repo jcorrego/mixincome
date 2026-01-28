7bffee7da6923ab42b15e574951f4d296a778b52
Juan Carlos Orrego <jcorrego@gmail.com>
2026-01-28 17:52:46 +0100

    Page Title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>MixIncome</title>
     <link rel="shortcut icon" href="/favicon.ico">
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
