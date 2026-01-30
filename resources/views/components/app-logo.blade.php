44e3329d683b68e31e2934a1a59b02cbb45ce4e3
Juan Carlos Orrego <jcorrego@gmail.com>
2026-01-30 02:29:20 +0100

    fix: increase logo to h-10

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -3,6 +3,6 @@
 ])
 
 <a href="{{ route('dashboard') }}" {{ $attributes->class('flex items-center') }} wire:navigate>
-    <img src="/images/logo-color.svg" alt="MixIncome" class="h-8 dark:hidden" />
-    <img src="/images/logo-white.svg" alt="MixIncome" class="hidden h-8 dark:block" />
+    <img src="/images/logo-color.svg" alt="MixIncome" class="h-10 dark:hidden" />
+    <img src="/images/logo-white.svg" alt="MixIncome" class="hidden h-10 dark:block" />
 </a>
