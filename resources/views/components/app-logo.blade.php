c894dd64068e65cc8ecc9093d8f9b737f62581f6
Juan Carlos Orrego <jcorrego@gmail.com>
2026-01-30 02:25:47 +0100

    fix: increase logo size from h-6 to h-8

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -4,12 +4,12 @@
 
 @if($sidebar)
     <flux:sidebar.brand {{ $attributes }}>
-        <img src="/images/logo-color.svg" alt="MixIncome" class="h-6 dark:hidden" />
-        <img src="/images/logo-white.svg" alt="MixIncome" class="hidden h-6 dark:block" />
+        <img src="/images/logo-color.svg" alt="MixIncome" class="h-8 dark:hidden" />
+        <img src="/images/logo-white.svg" alt="MixIncome" class="hidden h-8 dark:block" />
     </flux:sidebar.brand>
 @else
     <flux:brand {{ $attributes }}>
-        <img src="/images/logo-color.svg" alt="MixIncome" class="h-6 dark:hidden" />
-        <img src="/images/logo-white.svg" alt="MixIncome" class="hidden h-6 dark:block" />
+        <img src="/images/logo-color.svg" alt="MixIncome" class="h-8 dark:hidden" />
+        <img src="/images/logo-white.svg" alt="MixIncome" class="hidden h-8 dark:block" />
     </flux:brand>
 @endif
