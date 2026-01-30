268a1c91284a669ded9315a69f7681a11ad0b756
Juan Carlos Orrego <jcorrego@gmail.com>
2026-01-30 03:25:14 +0100

    Update mail template

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,9 +2,29 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block; text-decoration: none;">
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" width="48" height="48" style="vertical-align: middle;">
-    <circle cx="32" cy="32" r="28" fill="none" stroke="#1e293b" stroke-width="4" />
-    <text x="32" y="42" text-anchor="middle" fill="#1e293b" font-size="28" font-weight="bold" font-family="-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif">M</text>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 140 140" width="48" height="48" style="vertical-align: middle;">
+    <g>
+        <path d="M22.27,22.27C34.924,9.616 52.105,2.499 70,2.499C87.895,2.499 105.076,9.616 117.73,22.27" style="fill:none;fill-rule:nonzero;stroke:rgb(15,98,254);stroke-width:5px;"/>
+        <path d="M117.73,22.27C130.384,34.924 137.501,52.105 137.501,70C137.501,87.895 130.384,105.076 117.73,117.73" style="fill:none;fill-rule:nonzero;stroke:rgb(0,212,170);stroke-width:5px;"/>
+        <path d="M111.37,111.37C100.403,122.339 85.511,128.509 70,128.509C54.489,128.509 39.597,122.339 28.63,111.37" style="fill:none;fill-rule:nonzero;stroke:rgb(255,107,107);stroke-width:3px;"/>
+        <path d="M28.63,111.37C17.661,100.403 11.491,85.511 11.491,70C11.491,54.489 17.661,39.597 28.63,28.63" style="fill:none;fill-rule:nonzero;stroke:rgb(0,212,170);stroke-opacity:0.4;stroke-width:3px;"/>
+    </g>
+    <g transform="matrix(0.852941,0,0,0.875,10.294118,9.75)">
+        <clipPath id="_clip1">
+            <path d="M36,102L36,38L48.138,38L70,69.714L91.862,38L104,38L104,102L87.655,102L87.655,70L75.338,87.189L64.138,87.714L52.345,70L52.345,102L36,102Z" clip-rule="nonzero"/>
+        </clipPath>
+        <g clip-path="url(#_clip1)">
+            <g transform="matrix(1.172414,-0,-0,1.142857,-12.068966,-11.142857)">
+                <rect x="41" y="43" width="58" height="56" style="fill:url(#_Linear1);"/>
+            </g>
+        </g>
+    </g>
+    <defs>
+        <linearGradient id="_Linear1" x1="0" y1="0" x2="1" y2="0" gradientUnits="userSpaceOnUse" gradientTransform="matrix(58,0,0,56,41,71)">
+            <stop offset="0" style="stop-color:rgb(0,212,170);stop-opacity:1"/>
+            <stop offset="1" style="stop-color:rgb(15,98,254);stop-opacity:1"/>
+        </linearGradient>
+    </defs>
 </svg>
 <span style="display: inline-block; vertical-align: middle; margin-left: 8px; font-size: 22px; font-weight: 700; color: #1e293b; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif;">
     MixIncome
