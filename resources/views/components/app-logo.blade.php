9704a142da433fe4cfaec25cd469accd29d181d5
Juan Carlos Orrego <jcorrego@gmail.com>
2026-01-30 02:27:06 +0100

    fix: replace flux:sidebar.brand with plain link to avoid overflow clipping
    Flux brand component has h-6 overflow-hidden that clips the logo.

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -2,14 +2,7 @@
     'sidebar' => false,
 ])
 
-@if($sidebar)
-    <flux:sidebar.brand {{ $attributes }}>
-        <img src="/images/logo-color.svg" alt="MixIncome" class="h-8 dark:hidden" />
-        <img src="/images/logo-white.svg" alt="MixIncome" class="hidden h-8 dark:block" />
-    </flux:sidebar.brand>
-@else
-    <flux:brand {{ $attributes }}>
-        <img src="/images/logo-color.svg" alt="MixIncome" class="h-8 dark:hidden" />
-        <img src="/images/logo-white.svg" alt="MixIncome" class="hidden h-8 dark:block" />
-    </flux:brand>
-@endif
+<a href="{{ route('dashboard') }}" {{ $attributes->class('flex items-center') }} wire:navigate>
+    <img src="/images/logo-color.svg" alt="MixIncome" class="h-8 dark:hidden" />
+    <img src="/images/logo-white.svg" alt="MixIncome" class="hidden h-8 dark:block" />
+</a>
